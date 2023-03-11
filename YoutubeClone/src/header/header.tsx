2f3f100cabb9502fa6a2a7d338abbf7b5d0714d9
Sanidhya Koranne <sanidhyakoranne123@gmail.com>
2023-03-11 15:11:56 +0530

    added header component

---
+++ YoutubeClone/src/header/header.tsx
@@ -0,0 +1,8 @@
+function HeaderComponent (){
+    return(
+        <div>
+            <p>i am youtube header</p>
+        </div>
+    )
+}
+export default HeaderComponent;
--- YoutubeClone/src/main.tsx
+++ YoutubeClone/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import HeaderComponent from './header/header'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HeaderComponent />
   </React.StrictMode>,
 )
